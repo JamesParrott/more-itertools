d2fd76edaf97cf6c8a517ed35d0f06989c49dd33
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-07-20 12:17:39 +0100

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,39 @@
+import math
+from itertools import repeat, chain, cycle
+from typing import Sequence, Iterator
+
+import more_itertools as mit
+
+def zip_lcm[T](
+    seq0: Sequence[T],
+    seq1: Sequence[T],
+    *seqs: Sequence[T],
+) -> Iterator[tuple[T, ...]]:
+    lcm = math.lcm(*map(len, (seq0, seq1, *seqs)))
+    repeated_seq0 = repeat(seq0, lcm // len(seq0))
+    yield from zip(
+        chain.from_iterable(repeated_seq0),
+        cycle(seq1),
+        *map(cycle, seqs),
+    )
+
+
+def test(func_to_test=zip_lcm):
+    def helper(*seqs: str) -> str:
+        return " ".join(map("".join, func_to_test(*seqs)))
+
+    assert helper("01", "abc") == "0a 1b 0c 1a 0b 1c"
+    assert (
+        helper("01", "abcd", "xyz") == "0ax 1by 0cz 1dx 0ay 1bz 0cx 1dy 0az 1bx 0cy 1dz"
+    )
+
+    print("all tests pass")
+
+def alternative(*seqs):
+    lcm: int = math.lcm(*map(len, seqs))
+    yield from mit.take(lcm, zip(*map(cycle,seqs)))
+
+
+if __name__ == '__main__':
+    test()
+    test(alternative)
